69add6b539ec84ffdfb9b964b1f581893bd2b83c
neelaakhil12 <neelaakhiharish@gmail.com>
2026-06-29 11:52:25 +0530

    build: optimize next config for low-memory AWS server

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1
+  }
 };
 
 export default nextConfig;
